8328033952bc470695e2781ad233513f1dbb675d
abhi162002 <abhi162002pandey@gmail.com>
2023-12-12 00:19:57 +0530

    11_DEC_2023

---
+++ server.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import morgan from'morgan';
+import authRoutes from'./routes/authRoute.js';
+import connectDB from './config/db.js';
+import cors from 'cors';
+import categoryRoutes from './routes/categoryRoutes.js';
+import ProductRoute from './routes/ProductRoute.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+//configure env
+dotenv.config();
+
+//database config
+connectDB();
+
+//esmodule url
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
+
+//rest object
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+//routes
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", ProductRoute);
+
+//rest api
+app.use('*', function(req, res){
+    res.sendFile(path.join(__dirname, "./client/build/index.html"))
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on ${PORT}`);
+});
